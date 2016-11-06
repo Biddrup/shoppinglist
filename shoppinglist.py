de45a45b80073d5cd3320fd231cf7ae70f48ef14
Biddrup Kumar Mallick <Biddrup@users.noreply.github.com>
2016-11-07 01:01:50 +0600

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,32 +1,40 @@
-#This wii keep the database
+#This is a database for shopping list
+def printshoppinglist(shopdic):
+    print('Shopping List'.center(35, '='))
+    for k, v in shopdic.items():
+        print(k.ljust(20, '.') + 'TK. ' + str(v))
+        total = 0
+        total = total + int(v)
+        print('Total cost = ' + str(total))
+
+
 shoppinglist = {}
 
-#This will help you to find out and
+#This will help you to find out shopping items and
 #Update the database
 while True:
     #This will take input from user
-    print('Enter a name: (blank to quit)')
-    #keep in mind input is casesensetive
-    #so, 'Biddrup' and 'biddrup' is different from each other
+    print('Enter an item: (blank to quit)')
+    #This is not case sensetive
     name = input()
+
     #If input '', then loop will be terminated
     if name == '':
         break
-    
+
+    #This will keep the input as uppercase
     newname = name.upper()
-    #This will check the name in databse or not
-     
+    
+    #This will check the name in databse or not 
     if newname in shoppinglist:
         #If the input in databse, this will print
-        print (shoppinglist[newname] + ' is needed for this picnic.')
+        print('Budget for ' + newname + ' is TK. ' + shoppinglist[newname])
     else:
-        print('I do not have the information for ' + name)
+        print('I do not have the information for ' + newname)
         #This will take info under that name
         print('What is the amount')
         items = input()
-        newitems = items.upper()
-        shoppinglist[name] = newitems
+        shoppinglist[newname] = items
         #And, databse is updated now
-        print('Bazzaritems database updated')
-for i in shoppinglist.items():
-    print(i)
+        print('Shoppinglist database updated')
+printshoppinglist(shoppinglist)
